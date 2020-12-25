7fb4b8f8c8b33cfaf861644972b5507789ae96fb
JosueDLA <josuedlavs@gmail.com>
2020-12-25 14:10:28 -0600

    Add products model

--- order_control/admin.py
+++ order_control/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import UserType, User, Product
 # Register your models here.
+
+admin.site.register(UserType)
+admin.site.register(User)
+admin.site.register(Product)
--- order_control/models.py
+++ order_control/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+
+
+class UserType(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone = PhoneField(blank=True, help_text='Contact phone number')
+    user_type = models.ForeignKey(UserType, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    stock = models.IntegerField()
